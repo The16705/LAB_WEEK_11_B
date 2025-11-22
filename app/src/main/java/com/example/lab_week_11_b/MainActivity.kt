dce9d7b993af976043c5a8a16937e0c45ccabe62
The16705 <theodorusfernando6705@gmail.com>
2025-11-22 17:47:52 +0700

    Commit No. 2: Photo and Video using FileProvider and MediaStore

--- app/src/main/java/com/example/lab_week_11_b/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_11_b/MainActivity.kt
@@ -16,19 +16,14 @@ import androidx.core.view.WindowInsetsCompat
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
-    // Request code for permission request to external storage
     companion object {
         private const val REQUEST_EXTERNAL_STORAGE = 3
     }
 
-    // Helper class to manage files in MediaStore
     private lateinit var providerFileManager: ProviderFileManager
-    // Data model for the file
     private var photoInfo: FileInfo? = null
     private var videoInfo: FileInfo? = null
-    // Flag to indicate whether the user is capturing a photo or video
     private var isCapturingVideo = false
-    // Activity result launcher to capture images and videos
     private lateinit var takePictureLauncher: ActivityResultLauncher<Uri>
     private lateinit var takeVideoLauncher: ActivityResultLauncher<Uri>
 
@@ -36,7 +31,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Initialize the ProviderFileManager
         providerFileManager = ProviderFileManager(
             applicationContext,
             FileHelper(applicationContext),
@@ -45,10 +39,6 @@ class MainActivity : AppCompatActivity() {
             MediaContentHelper()
         )
 
-        // Initialize the activity result launcher
-        // .TakePicture() and .CaptureVideo() are the built-in contracts
-        // They are used to capture images and videos
-        // The result will be stored in the URI passed to the launcher
         takePictureLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()) {
             providerFileManager.insertImageToStore(photoInfo)
         }
@@ -57,55 +47,43 @@ class MainActivity : AppCompatActivity() {
         }
 
         findViewById<Button>(R.id.photo_button).setOnClickListener {
-            // Set the flag to indicate that the user is capturing a photo
             isCapturingVideo = false
-            // Check the storage permission
-            // If the permission is granted, open the camera
-            // Otherwise, request the permission
             checkStoragePermission {
                 openImageCapture()
             }
         }
 
         findViewById<Button>(R.id.video_button).setOnClickListener {
-            // Set the flag to indicate that the user is capturing a video
             isCapturingVideo = true
-            // Check the storage permission
-            // If the permission is granted, open the camera
-            // Otherwise, request the permission
             checkStoragePermission {
                 openVideoCapture()
             }
         }
     }
 
-    // Open the camera to capture an image
     private fun openImageCapture() {
         photoInfo = providerFileManager.generatePhotoUri(System.currentTimeMillis())
-        takePictureLauncher.launch(photoInfo?.uri)
+        photoInfo?.uri?.let { uri ->
+            takePictureLauncher.launch(uri)
+        }
     }
 
-    // Open the camera to capture a video
     private fun openVideoCapture() {
         videoInfo = providerFileManager.generateVideoUri(System.currentTimeMillis())
-        takeVideoLauncher.launch(videoInfo?.uri)
+        videoInfo?.uri?.let { uri ->
+            takeVideoLauncher.launch(uri)
+        }
     }
 
-    // Check the storage permission
-    // For Android 10 and above, the permission is not required
-    // For Android 9 and below, the permission is required
     private fun checkStoragePermission(onPermissionGranted: () -> Unit) {
         if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.Q) {
-            // Check for the WRITE_EXTERNAL_STORAGE permission
             when (ContextCompat.checkSelfPermission(
                 this,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE
             )) {
-                // If the permission is granted
                 PackageManager.PERMISSION_GRANTED -> {
                     onPermissionGranted()
                 }
-                // if the permission is not granted, request the permission
                 else -> {
                     ActivityCompat.requestPermissions(
                         this,
@@ -119,8 +97,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // For android 9 and below
-    // Handle the permission request result
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
@@ -128,9 +104,7 @@ class MainActivity : AppCompatActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
-            // Check if requestCode is for the External Storage permission or not
             REQUEST_EXTERNAL_STORAGE -> {
-                // If granted, open the camera
                 if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
                     if (isCapturingVideo) {
                         openVideoCapture()
@@ -140,7 +114,6 @@ class MainActivity : AppCompatActivity() {
                 }
                 return
             }
-            // for other request code, do nothing
             else -> {
             }
         }
--- app/src/main/java/com/example/lab_week_11_b/ProviderFileManager.kt
+++ app/src/main/java/com/example/lab_week_11_b/ProviderFileManager.kt
@@ -1,20 +1,21 @@
 package com.example.lab_week_11_b
 
-// Helper class to manage files in MediaStore
+import android.content.ContentResolver
+import android.content.ContentValues
+import android.content.Context
+import android.net.Uri
+import java.io.File
+import java.util.concurrent.Executor
+
 class ProviderFileManager(
     private val context: Context,
     private val fileHelper: FileHelper,
     private val contentResolver: ContentResolver,
     private val executor: Executor,
-    private val mediaContentHelper: MediaContentHelper) {
-// Generate the data model (FileInfo) for the file
-// The data model contains the URI, file, name, relative path,
-    and MIME type of the file
+    private val mediaContentHelper: MediaContentHelper
+) {
     fun generatePhotoUri(time: Long): FileInfo {
         val name = "img_$time.jpg"
-// Get the file object
-// The file will be stored in the folder defined in
-        file_provider_paths.xml
         val file = File(
             context.getExternalFilesDir(fileHelper.getPicturesFolder()),
             name
@@ -27,11 +28,9 @@ class ProviderFileManager(
             "image/jpeg"
         )
     }
+
     fun generateVideoUri(time: Long): FileInfo {
         val name = "video_$time.mp4"
-// Get the file object
-// The file will be stored in the folder defined in
-        file_provider_paths.xml
         val file = File(
             context.getExternalFilesDir(fileHelper.getVideosFolder()),
             name
@@ -44,15 +43,17 @@ class ProviderFileManager(
             "video/mp4"
         )
     }
-    // Insert the image/video to MediaStore
+
     fun insertImageToStore(fileInfo: FileInfo?) {
         fileInfo?.let {
-            insertToStore(fileInfo,
+            insertToStore(
+                fileInfo,
                 mediaContentHelper.getImageContentUri(),
                 mediaContentHelper.generateImageContentValues(it)
             )
         }
     }
+
     fun insertVideoToStore(fileInfo: FileInfo?) {
         fileInfo?.let {
             insertToStore(
@@ -62,21 +63,18 @@ class ProviderFileManager(
             )
         }
     }
-    // Insert the file to MediaStore
-// The file will be copied to the given relative path
-// Input Stream is used to read the file
-// Output Stream is used to write the file
-    private fun insertToStore(fileInfo: FileInfo, contentUri: Uri,
-                              contentValues: ContentValues) {
+
+    private fun insertToStore(fileInfo: FileInfo, contentUri: Uri, contentValues: ContentValues) {
         executor.execute {
-            val insertedUri = contentResolver.insert(contentUri,
-                contentValues)
+            val insertedUri = contentResolver.insert(contentUri, contentValues)
             insertedUri?.let {
-                val inputStream =
-                    contentResolver.openInputStream(fileInfo.uri)
-                val outputStream =
-                    contentResolver.openOutputStream(insertedUri)
-                IOUtils.copy(inputStream, outputStream)
+                val inputStream = contentResolver.openInputStream(fileInfo.uri)
+                val outputStream = contentResolver.openOutputStream(insertedUri)
+                inputStream?.use { input ->
+                    outputStream?.use { output ->
+                        input.copyTo(output)
+                    }
+                }
             }
         }
     }
